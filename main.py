51f0069c72c830a5c87e2bc5babd99d97108c1f9
Adam Shugar <ashugar@stanford.edu>
2020-12-22 05:26:01 -0800

    Fix import err

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import multiprocessing as mp
 import gui
 import gui.hdpi as hdpi
-import util
+from util import is_windows
 from gui.paraminput import GasList, ShortEntryList, CheckbuttonList
 from gui.filepick import FileList
 
